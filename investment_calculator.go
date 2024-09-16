3f3e98d599a8a27a440a55814f770ecbcc479ba7
Tan Jun Wei <junwei2509@gmail.com>
2024-09-16 23:31:45 +0800

    Variables and console print

---
+++ investment_calculator.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	//investmentAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5
+	const inflationRate = 6.5
+	var investmentAmount float64 = 1000
+	years := 10.0
+	expectedReturnRate := 5.5
+
+	futureValue := investmentAmount * math.Pow(1+expectedReturnRate/100, years)
+
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
+	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
+}
