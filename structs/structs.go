8442eee07ccb25284aaa53977a4334120710743f
Tan Jun Wei <junwei2509@gmail.com>
2024-09-27 23:36:08 +0800

    Structs: Constructor function

--- structs/structs.go
+++ structs/structs.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"time"
 )
@@ -12,20 +13,47 @@ type user struct {
 	createdAt time.Time
 }
 
+// func outputUserDetails(u *user) {
+// 	fmt.Println(u.firstName, u.lastName, u.birthDate)
+// }
+
+// Method with value receiver
+func (u *user) outputUserDetails() {
+	fmt.Println(u.firstName, u.lastName, u.birthDate)
+}
+
+// Mutation methods, using a pointer to edit value
+func (u *user) clearUserName() {
+	u.firstName = ""
+	u.lastName = ""
+}
+
+func newUser(firstName, lastName, birthDate string) (*user, error) {
+	if firstName == "" || lastName == "" || birthDate == "" {
+		return nil, errors.New("First name, last name, birthdate are required.")
+	}
+
+	return &user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthDate: birthDate,
+		createdAt: time.Now(),
+	}, nil
+}
+
 func main() {
 	userFirstName := getUserData("Please enter your first name: ")
 	userLastName := getUserData("Please enter your last name: ") //cant handle spaces
 	userBirthdate := getUserData("Please enter your birthdate (MM/DD/YYY): ")
 
-	var appUser user
+	var appUser *user
 
-	appUser = user{
-		firstName: userFirstName,
-		lastName:  userLastName,
-		birthDate: userBirthdate,
-		createdAt: time.Now(),
+	appUser, err := newUser(userFirstName, userLastName, userBirthdate)
+
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
-
 	// appUser = user{
 	// 	userFirstName,
 	// 	userLastName,
@@ -33,16 +61,14 @@ func main() {
 	// 	time.Now(),
 	// }
 
-	outputUserDetails(&appUser)
-}
-
-func outputUserDetails(u *user) {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
+	appUser.outputUserDetails()
+	appUser.clearUserName()
+	appUser.outputUserDetails()
 }
 
 func getUserData(promptText string) string {
 	fmt.Print(promptText)
 	var value string
-	fmt.Scan(&value)
+	fmt.Scanln(&value)
 	return value
 }
