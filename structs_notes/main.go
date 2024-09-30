40ea7fac6b0369baff584d66b3d96907b7b41521
Tan Jun Wei <junwei2509@gmail.com>
2024-09-30 23:22:27 +0800

    Structs_notes: Embedded Interfaces

--- structs_notes/main.go
+++ structs_notes/main.go
@@ -14,6 +14,20 @@ type saver interface {
 	Save() error
 }
 
+// type displayer interface {
+// 	Display()
+// }
+
+type outputtable interface {
+	saver
+	Display()
+}
+
+// type outputtable interface {
+// 	Save() error
+// 	Display()
+// }
+
 func main() {
 	title, content := getNoteData()
 
@@ -33,21 +47,18 @@ func main() {
 		return
 	}
 
-	todo.Display()
-	err = saveData(todo)
+	err = outputData(todo)
 
 	if err != nil {
 		return
 	}
 
-	fmt.Println("Saving the todo succeeded!")
+	outputData(userNote)
+}
 
-	userNote.Display()
-	err = saveData(userNote)
-
-	if err != nil {
-		return
-	}
+func outputData(data outputtable) error {
+	data.Display()
+	return saveData(data)
 }
 
 func saveData(data saver) error {
