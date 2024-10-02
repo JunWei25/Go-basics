f6f3e114cf676c44a655f6fe8c772fc8214a8b60
Tan Jun Wei <junwei2509@gmail.com>
2024-10-02 21:23:35 +0800

    Structs_notes: extracting type information from values

--- structs_notes/main.go
+++ structs_notes/main.go
@@ -40,6 +40,9 @@ func main() {
 		return
 	}
 
+	// not working because it is not a recognized type by Go
+	printSomething(todo)
+
 	userNote, err := note.New(title, content)
 
 	if err != nil {
@@ -58,7 +61,38 @@ func main() {
 
 // interface{} allows any data type
 func printSomething(value interface{}) {
-	fmt.Println(value)
+	intVal, ok := value.(int)
+
+	if ok {
+		fmt.Println("Integer:", intVal)
+		return
+	}
+
+	floatVal, ok := value.(float64)
+
+	if ok {
+		fmt.Println("Float:", floatVal)
+		return
+	}
+
+	stringVal, ok := value.(string)
+
+	if ok {
+		fmt.Println("String", stringVal)
+		return
+	}
+
+	// switch value.(type) {
+	// case int:
+	// 	fmt.Println("Integer:", value)
+	// case float64:
+	// 	fmt.Println("Float:", value)
+	// case string:
+	// 	fmt.Println("String:", value)
+	// default:
+	// 	//fallback case
+	// }
+	// fmt.Println(value)
 }
 
 func outputData(data outputtable) error {
