d0847de3411d3170b7995acb2a50fcfe42dc3280
Tan Jun Wei <junwei2509@gmail.com>
2024-10-06 23:54:32 +0800

    Arrays: append and practice

--- list/list.go
+++ list/list.go
@@ -2,30 +2,43 @@ package main
 
 import "fmt"
 
-type Product struct {
-	title string
-	id    string
-	price float64
-}
-
 func main() {
-	var productNames [4]string = [4]string{"A Book"}
-	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1])
+	prices[1] = 9.99
+	prices[2] = 11.99
 
-	productNames[2] = "A Carpet"
+	updatedPrices := append(prices, 5.99)
+	fmt.Println(updatedPrices)
 
+	prices = append(prices, 5.99)
 	fmt.Println(prices)
-	fmt.Println(productNames)
-
-	featuredPrices := prices[1:]
-	featuredPrices[0] = 199.99
-	highlightedPrices := featuredPrices[:1]
-	fmt.Println(highlightedPrices)
-	fmt.Println(prices)
-	fmt.Println(len(featuredPrices), cap(featuredPrices))
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
-
-	highlightedPrices = highlightedPrices[:3]
-	fmt.Println(highlightedPrices)
-	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
 }
+
+// type Product struct {
+// 	title string
+// 	id    string
+// 	price float64
+// }
+
+// func main() {
+// 	var productNames [4]string = [4]string{"A Book"}
+// 	prices := [4]float64{10.99, 9.99, 45.99, 20.0}
+
+// 	productNames[2] = "A Carpet"
+
+// 	fmt.Println(prices)
+// 	fmt.Println(productNames)
+
+// 	featuredPrices := prices[1:]
+// 	featuredPrices[0] = 199.99
+// 	highlightedPrices := featuredPrices[:1]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(prices)
+// 	fmt.Println(len(featuredPrices), cap(featuredPrices))
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+
+// 	highlightedPrices = highlightedPrices[:3]
+// 	fmt.Println(highlightedPrices)
+// 	fmt.Println(len(highlightedPrices), cap(highlightedPrices))
+// }
