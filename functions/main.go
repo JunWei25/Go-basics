3b4c260c6f070899352ac4a1bdccb2089111bcf2
Tan Jun Wei <junwei2509@gmail.com>
2024-10-09 23:49:14 +0800

    Function: Returning functions as value

--- functions/main.go
+++ functions/main.go
@@ -10,11 +10,21 @@ type anotherFn func(int, []string, map[string][]int) ([]int, string)
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformerFn1 := getTransformerFunction(&numbers)
+	transformerFn2 := getTransformerFunction(&moreNumbers)
+
+	transformNumbers := transformNumbers(&numbers, transformerFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformerFn2)
+
+	fmt.Println(transformNumbers)
+	fmt.Println(moreTransformedNumbers)
 }
 
 func transformNumbers(numbers *[]int, transform transformFn) []int {
@@ -25,6 +35,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 	}
 
 	return dNumbers
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
